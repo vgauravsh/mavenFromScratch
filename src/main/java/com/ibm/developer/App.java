7a8060becfff867bfdcda067fec2b8ed74248487
Gaurav Vishwanath <gaurav.vishwanath@talentica.com>
2016-01-17 12:56:52 +0530

    modified maven project

---
+++ src/main/java/com/ibm/developer/App.java
@@ -0,0 +1,37 @@
+package com.ibm.developer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    package com.ibm.developer; 
+
+import java.io.IOException; 
+import java.net.ServerSocket; 
+import java.net.Socket; 
+
+/** 
+* Hello world! 
+ * 
+ */ 
+public class App { 
+{
+ public static void main(String[] args) throws IOException { 
+        int port = Integer.parseInt(System.getenv("VCAP_APP_PORT")); 
+        ServerSocket ss = new ServerSocket(port); 
+        while (true) { 
+            Socket s = ss.accept(); 
+            s.getOutputStream() 
+                    .write(("HTTP/1.1 200 OK\nContent-Type:
+		text/html; charset=utf-8\nContent-Length: 2\n\n" +
+		veryComplicatedMethod()) .getBytes()); 
+        } 
+    } 
+
+    public static int veryComplicatedMethod() { 
+        return 42; 
+    } 
+
+}
